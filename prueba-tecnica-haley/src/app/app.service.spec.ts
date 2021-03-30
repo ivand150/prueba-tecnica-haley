305d7f722da8ed15f0fed72ee3e83ec7c4e97896
Ivan Dobry <ivandobry@hotmail.es>
2021-03-30 18:19:54 +0200

    service tested

--- prueba-tecnica-haley/src/app/app.service.spec.ts
+++ prueba-tecnica-haley/src/app/app.service.spec.ts
@@ -13,4 +13,22 @@ describe('AppService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy()
   })
+
+  it('setArray$.next should be called', () => {
+    spyOn(service.sortArray$, 'next').and.callThrough()
+    service.setSortArray([])
+    expect(service.sortArray$.next).toHaveBeenCalled()
+  })
+
+  it('unidimensionalArray$.next should be called', () => {
+    spyOn(service.unidimensionalArray$, 'next').and.callThrough()
+    service.setUnidimensionalArray([])
+    expect(service.unidimensionalArray$.next).toHaveBeenCalled()
+  })
+
+  it('wordsCount$.next should be called', () => {
+    spyOn(service.wordsCount$, 'next').and.callThrough()
+    service.setWordsCount(2)
+    expect(service.wordsCount$.next).toHaveBeenCalled()
+  })
 })
